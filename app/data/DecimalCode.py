ba6b8a1b5532d1ad916588626732902f7ada6b10
ModischFabrications <modisch.fabrications@gmail.com>
2019-11-18 18:18:33 +0100

    add data class tests

--- app/data/DecimalCode.py
+++ app/data/DecimalCode.py
@@ -6,5 +6,13 @@ class DecimalCode:
     # could be a str instead
     code: int
 
+    def __post_init__(self):
+        self.assert_valid()
+
+    def assert_valid(self):
+        # TODO: extend with proper tests (how?)
+        if not self.code > 4:
+            raise SyntaxError(f"Code [{self.code}] is invalid")
+
     def __str__(self) -> str:
         return f"{self.code}"
--- app/data/DipCode.py
+++ app/data/DipCode.py
@@ -1,4 +1,8 @@
+import re
 from dataclasses import dataclass
+
+system_pattern = re.compile('[01]{5}')
+device_pattern = re.compile('[1-5]')
 
 
 @dataclass(frozen=True)
@@ -7,6 +11,15 @@ class DipCode:
     system: str
     device: str
     state: bool
+
+    def __post_init__(self):
+        self.assert_valid()
+
+    def assert_valid(self):
+        if not system_pattern.match(self.system):
+            raise SyntaxError(f"System code [{self.system}] is invalid")
+        if not device_pattern.match(self.device):
+            raise SyntaxError(f"device code [{self.device}] is invalid")
 
     def __str__(self) -> str:
         return f"{self.system}, {self.device}, {int(self.state)}"
